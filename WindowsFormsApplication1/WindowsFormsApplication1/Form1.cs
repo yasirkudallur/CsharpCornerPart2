1b4724a4e69349c105f6d9a5099f04d99a52e3b8
Mohamed Yasir <yasirkudallur@gmail.com>
2020-12-28 15:01:44 +0400

    Commit CHnages in Form Load

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -15,5 +15,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
